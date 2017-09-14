5746dc23489ef5e07b5c931c2472ae12e854303b
Christian Hammervig <christian@hammervig.dk>
2017-09-14 10:36:50 +0200

    First Commit

---
+++ Lab/Starter/PhotoSharingApplication/PhotoSharingApplication/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using PhotoSharingApplication.Models;
+
+namespace PhotoSharingApplication.Controllers
+{
+	[Authorize]
+    public class AccountController : Controller
+    {
+		// GET: Account
+		[AllowAnonymous]
+		public ActionResult Login(string returnUrl) {
+			ViewBag.ReturnUrl = returnUrl;
+			return View();
+		}
+
+		[AllowAnonymous]
+		[HttpPost]
+		public ActionResult Login(login model, string returnUrl) {
+			if (ModelState.IsValid) {
+				if (Membership.ValidateUser(model.UserName, model.Password)) {
+					FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+					if (Url.IsLocalUrl(returnUrl)) {
+						return Redirect(returnUrl);
+					} else {
+						return RedirectToAction("Index", "Home");
+					}
+				} else {
+					ModelState.AddModelError("", "The user name or password provided is incorrect.");
+				}
+			}
+
+			return View(model);
+		}
+		public ActionResult LogOff() {
+			FormsAuthentication.SignOut();
+			return RedirectToAction("Index", "Home");
+		}
+		public ActionResult Register() {
+			return View();
+		}
+	}
+}
---
+++ Lab/Starter/PhotoSharingApplication/PhotoSharingApplication/Models/AccountModelClasses.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.ComponentModel;
+
+namespace PhotoSharingApplication.Models {
+	public class AccountModelClasses {
+		public class UsersContext : DbContext {
+
+		}
+
+		
+	}
+	public class login {
+		[Required]
+		[DisplayName("User Name")]
+		public string UserName { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[DisplayName("Password")]
+		public string Password { get; set; }
+
+		[DisplayName("Remember Me?")]
+		public bool RememberMe { get; set; }
+
+	}
+
+	public class Register {
+		[Required]
+		[DisplayName("User Name")]
+		public string UserName { get; set; }
+
+		[Required]
+		[StringLength(100, ErrorMessage = "The {0} must be minimum {2} charaters long.", MinimumLength = 6)]
+		[DataType(DataType.Password)]
+		[DisplayName("Password")]
+		public string Password { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[DisplayName("Confirm Password")]
+		[Compare("Password", ErrorMessage = "Passwords does not match...")]
+		public string ConfirmPassword { get; set; }
+	}
+}
